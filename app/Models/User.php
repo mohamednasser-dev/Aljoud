af6dedeb13b1c5c930c42fa4b328c6da6962be6f
MostafaElebzary <mostafaelebzary@gmail.com>
2021-11-11 22:26:28 +0200

    m

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -38,5 +39,41 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+
+    public function getImageAttribute($image)
+    {
+        if (!empty($image)){
+            return asset('uploads/admins').'/'.$image;
+        }
+        return asset('uploads/admins/default.jpg');
+    }
+
+    public function setImageAttribute($image)
+    {
+
+        if (is_file($image)) {
+            $imageFields = upload($image, 'admins');
+            $this->attributes['image'] = $imageFields;
+
+        }
+
+    }
+
+    public function setPasswordAttribute($password)
+    {
+        if (!empty($password)){
+            $this->attributes['password'] = Hash::make($password);
+        }
+    }
+
+    protected function castAttribute($key, $value)
+    {
+        if ($this->getCastType($key) == 'string' && is_null($value)) {
+            return '';
+        }
+
+
+        return parent::castAttribute($key, $value);
+    }
 
 }
