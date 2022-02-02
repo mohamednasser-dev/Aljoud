048aa16789a23b26d1c7bf42e5bf6d62ed5091e2
MostafaElebzary <mostafaelebzary@gmail.com>
2022-02-02 13:34:53 +0200

    max

--- app/Http/Controllers/Api/Admin/VideosController.php
+++ app/Http/Controllers/Api/Admin/VideosController.php
@@ -64,7 +64,7 @@ class VideosController extends Controller
                 $rules = [
                     'name_ar' => 'required',
                     'name_en' => 'required',
-                    'url' => 'required|file|mimes:mp4|max:102400',
+                    'url' => 'required|file|mimes:mp4',
                     'time' => 'required',
                     'lesson_id' => 'required|exists:lessons,id',
                 ];
